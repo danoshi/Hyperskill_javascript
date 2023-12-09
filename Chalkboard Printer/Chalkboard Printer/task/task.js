c95ce66f58ff18cb77f3b5c08ff051db4769aad4
danoshi <39885406+danoshi@users.noreply.github.com>
2023-12-09 21:57:23 +0100

    new project

---
+++ Chalkboard Printer/Chalkboard Printer/task/task.js
@@ -0,0 +1,22 @@
+// Welcome to the Chalkboard Printer!
+const input = require('sync-input');
+
+// These variables are already declared for you.
+// Looks like we made a mistake and used 'var' instead of 'let'!
+// Remember it's bad practice to use 'var', so let's change all of them to 'let'.
+
+// We have also imported the 'sync-input' package for you.
+// You will use this in later stages.
+// This package allows you to get user input.
+// Like so:
+
+function Output_Message(name, surname, message){
+    for (let i = 0; i < 5; i++){
+         console.log("This is " + name + " " + surname + " and " + message)
+    }
+}
+let name = input("Enter name: ");
+let surname = input("Enter surname: ");
+let message = input("Enter message: ");
+
+Output_Message(name, surname, message)
